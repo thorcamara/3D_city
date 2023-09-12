3f7fe652bd73d8a67849f19c5c2e44f311d5ed0c
thorcamara <thorcamaraalthoff@gmail.com>
2023-09-12 14:32:09 -0300

    Finished

--- script.js
+++ script.js
@@ -201,3 +201,36 @@ var generateLines = function () {
     createCars(0.1, 20);
   };
 };
+
+var cameraSet = function () {
+  createCars(0.1, 20, 0xFFFFFF);
+};
+
+var animate = function () {
+  var time = Date.now() * 0.00005;
+  requestAnimationFrame(animate);
+
+  city.rotation.y -= ((mouse.x * 8) - camera.rotation.y) * uSpeed;
+  city.rotation.x -= (-(mouse.y * 2) - camera.rotation.x) * uSpeed;
+  if (city.rotation.x < -0.05) {
+      city.rotation.x = -0.05;
+  } else if (city.rotation.x > 1) {
+      city.rotation.x = 1;
+  }
+  var cityRotation = Math.sin(Date.now() / 5000) * 13;
+  for (let i = 0, l = town.children.length; i < l; i++) {
+      var object = town.children[i];
+  }
+
+  smoke.rotation.y += 0.01;
+  smoke.rotation.x += 0.01;
+
+  camera.lookAt(city.position);
+  renderer.render(scene, camera);
+
+}
+
+
+generateLines();
+init();
+animate();
