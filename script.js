483f9ea8343b91a747deed821879517fd28b5ff2
thorcamara <thorcamaraalthoff@gmail.com>
2023-09-11 22:10:41 -0300

    Added more variables and functions

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ var smoke = new THREE.Object3D();
 var town = new THREE.Object3D();
 var createCarPos = true;
 var uSpeed = 0.001;
+
+var setcolor = 0xF02050;
+scene.background = new THREE.Color(setcolor);
+scene.fog = new THREE.Fog(setcolor, 10, 16);
+
+function mathRandom(num = 8) {
+    var numValue = - Math.random() * num + Math.random() * num;
+    return numValue;
+};
+
+var setTintNum = true;
+function setTintColor() {
+    if (setTintNum) {
+        setTintNum = false;
+        var setColor = 0x000000;
+    } else {
+        setTintNum = true;
+        var setColor = 0x000000;
+    };
+    return setColor;
+};
